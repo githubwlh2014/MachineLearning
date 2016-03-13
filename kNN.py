7e718bb5514ec07f849ccd089f98d00c84088b51
change2014 <812799024@qq.com>
2016-03-14 00:25:42 +0800

    handTest

--- kNN.py
+++ kNN.py
@@ -1,5 +1,6 @@
 #coding:utf-8
 from numpy import *
+from os import listdir
 import operator
 def createDataSet():
     group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
@@ -14,8 +15,8 @@ def classify0(inX, dataSet, labels, k):
     sortedDistIndicies = distances.argsort()
     classCount={} 
     for i in range(k):
-			voteIlabel = labels[sortedDistIndicies[i]]                  
-			classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1   
+        voteIlabel = labels[sortedDistIndicies[i]]                  
+	classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1   
     sortedClassCount = sorted(classCount.iteritems(), 
     key=operator.itemgetter(1), reverse=True)        
     return sortedClassCount[0][0]
@@ -44,4 +45,40 @@ def autoNorm(dataSet):
     normDataSet=dataSet-tile(minVals,(m,1))
     normDataSet=normDataSet/tile(ranges,(m,1))
     return normDataSet,ranges,minVals
+
+def img2vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+    return returnVect
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('F:/MachineLearning/machinelearninginaction/Ch02/trainingDigits')   
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]        
+        fileStr = fileNameStr.split('.')[0]             
+        classNumStr = int(fileStr.split('_')[0]) 
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('F:/MachineLearning/machinelearninginaction/Ch02/trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('F:/MachineLearning/machinelearninginaction/Ch02/testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('F:/MachineLearning/machinelearninginaction/Ch02/testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, \
+                                trainingMat, hwLabels, 3)
+        print "the classifier came back with: %d, the real answer is: %d"\
+                                % (classifierResult, classNumStr)
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print "\nthe total number of errors is: %d" % errorCount
+    print "\nthe total error rate is: %f" % (errorCount/float(mTest))
 
