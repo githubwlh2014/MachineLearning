16f4d72a684727cf79c58f80f229c6e4aae4c672
change2014 <812799024@qq.com>
2016-03-13 23:19:29 +0800

    test

--- kNN.py
+++ kNN.py
@@ -14,8 +14,8 @@ def classify0(inX, dataSet, labels, k):
     sortedDistIndicies = distances.argsort()
     classCount={} 
     for i in range(k):
-        voteIlabel = labels[sortedDistIndicies[i]]                  
-        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1   
+			voteIlabel = labels[sortedDistIndicies[i]]                  
+			classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1   
     sortedClassCount = sorted(classCount.iteritems(), 
     key=operator.itemgetter(1), reverse=True)        
     return sortedClassCount[0][0]
